d4543cfe8d6d54bf13bbd2eec198b659855f607b
Jacob Gillespie <jacobwgillespie@gmail.com>
2013-09-07 12:18:58 -0500

    Switch to callbacks, add tests

--- test/network.js
+++ test/network.js
@@ -1,22 +1,54 @@
 // test/network.js;
+/* global apiKey, describe, it, should */
 
-var chai = require('chai');
-chai.should();
+var TheEchoNest = require('../index');
 
-var Network = require('../lib/network');
+var demoUrl = 'song/profile?format=json&id=SOCZMFK12AC468668F';
 
 describe('Network', function() {
-  var network = new Network();
+  var echonest = new TheEchoNest(apiKey);
+  var network = echonest.network;
 
   it('should make a network request', function(done) {
-    network.get('http://developer.echonest.com/api/v4/song/profile?api_key=FILDTEOIK2HBORODV&format=json&id=SOCZMFK12AC468668F').then(function(response) {
-      //console.log(response);
-      var test = true;
-      test.should.equal(true);
+    network.get(demoUrl, function(err, data) {
+      should.not.exist(err);
+      should.exist(data);
       done();
-    }).fail(function(error) {
-      console.error(error);
     });
+  });
+
+  it('should handle rate limits', function(done) {
+    echonest.setApiKey('FILDTEOIK2HBORODV');
+    this.timeout(10000);
+
+    var finished = false;
+
+    var fn = function(err, response) {
+      if (finished) return;
+
+      if (err) {
+        echonest.setApiKey(apiKey);
+        done();
+        finished = true;
+        return;
+      }
+
+      response.headers.should.have.property('x-ratelimit-limit');
+      response.headers.should.have.property('x-ratelimit-used');
+      response.headers.should.have.property('x-ratelimit-remaining');
+
+      var limit = parseInt(response.headers['x-ratelimit-limit'], 10);
+      var used = parseInt(response.headers['x-ratelimit-used'], 10);
+      var remaining = parseInt(response.headers['x-ratelimit-remaining'], 10);
+
+      used.should.be.below(limit);
+      remaining.should.be.above(0);
+    };
+
+    for (var i = 0; i < 10; i++) {
+      network.get(demoUrl, fn);
+    }
+
   });
 });
 
---
+++ test/support/common.js
@@ -0,0 +1,20 @@
+// test/support/common.js
+
+var chai = require('chai'),
+    chaiAsPromised = require('chai-as-promised');
+
+chai.use(chaiAsPromised);
+
+global.should = chai.should();
+global.expect = chai.expect;
+global.check = function check(done, f) {
+  try {
+    f();
+    done();
+  } catch(e) {
+    done(e);
+  }
+};
+
+// use custom API key or the demo one from the docs
+global.apiKey = process.env.ECHONEST_API_KEY || 'FILDTEOIK2HBORODV';
